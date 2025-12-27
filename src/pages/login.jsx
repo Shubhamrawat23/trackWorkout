8efe1ad37da15cdb5b41ebe530de4c485e7a1d3c
Shubhamrawat23 <shubhamrawat2311@gmail.com>
2025-12-27 20:43:35 +0530

    update store and add data on store while signup and login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,23 +3,70 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import {
-  Dialog,
-  DialogDescription,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
+    Dialog,
+    DialogDescription,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
 } from "@/components/ui/dialog";
 import { useWktStore } from "@/store";
+import supabase from "@/lib/supabaseClient";
 
 
-export default function LogIn({isOpen}) {
+export default function LogIn({ isOpen }) {
     const toggleBox = useWktStore((state) => state.toggleLoginBox);
-    const toggleSignupBox = useWktStore((state) => state.toggleSignupBox)
-    
+    const toggleSignupBox = useWktStore((state) => state.toggleSignupBox);
+    const setUserDetails = useWktStore((state)=>state.setUserDetails)
+    let userSignUpData = useWktStore((state) => state.user_Data)
+
     const signUpClick = () => {
         toggleBox();
         toggleSignupBox();
     };
+
+    
+    const loginForm = async function (resp) {
+        resp.preventDefault();
+        
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: resp.target[0].value,
+            password: resp.target[1].value,
+        })
+        
+        if (!error) {
+            // console.log(data);
+            document.querySelector('.login_err').innerText = "";
+
+            if (data?.session) {
+                let token = data?.session?.access_token;
+                let email_id = data?.user?.email;
+                let phone = data?.user?.user_metadata?.phone_number;
+                let country_code = data?.user?.user_metadata?.country_code;
+                let user_name = data?.user?.user_metadata?.user_name;
+
+                let gmt_created_date = new Date(data?.user?.created_at);
+
+                let created_at = gmt_created_date.toLocaleDateString("en",{
+                    month:'long',
+                    year:'numeric',
+                    day:'numeric'
+                });
+                
+
+                setUserDetails({'token': token, 'email_id':email_id, 'phone_number':phone, 'country_code':country_code, 'user_name':user_name, 'created_date':created_at});
+
+                toggleBox()
+                
+            }
+            
+        }else{
+            console.log(error);            
+            document.querySelector('.login_err').innerText = "Error in login";
+            
+        }
+        
+        
+    }
     return (
         <>
             <Dialog open={isOpen} onOpenChange={toggleBox}>
@@ -33,7 +80,7 @@ export default function LogIn({isOpen}) {
                         </DialogDescription>
                     </DialogHeader>
 
-                    <form className="space-y-4 mt-4">
+                    <form className="space-y-4 mt-4" onSubmit={(e)=>loginForm(e)}>
                         <div>
                             <Label htmlFor="loginEmail" className="my-2">Email</Label>
                             <Input
@@ -58,9 +105,10 @@ export default function LogIn({isOpen}) {
                             />
                         </div>
 
-                        <Button className="w-full" type="submit">
+                        <Button className="w-full mb-0" type="submit">
                             Login
                         </Button>
+                        <small className="text-red-700 login_err font-medium text-sm"></small>
                     </form>
 
                     <p className="text-sm text-center text-muted-foreground mt-3">
--- src/pages/main_page.jsx
+++ src/pages/main_page.jsx
@@ -6,11 +6,11 @@ import Signup from "./signup";
 
 export default function Main_page(){
     const isloginBoxShow = useWktStore((state) => state.isloginBoxShow);
-    const isSignupBoxShow = useWktStore((state) => state.isSignupBoxShow);
+    // const isSignupBoxShow = useWktStore((state) => state.isSignupBoxShow);
     return (
         <>
             <LogIn isOpen={isloginBoxShow}></LogIn>
-            <Signup isOpen={isSignupBoxShow}></Signup>
+            <Signup></Signup>
         </>
     )
 }
